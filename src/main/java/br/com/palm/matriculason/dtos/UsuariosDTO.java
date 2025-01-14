85dcd70ad94075ad94f54317c0a58a9261fb8032
Thalisson <thalissonmoura138@gmail.com>
2025-01-14 17:57:16 -0300

    :recycle:refactor:  Ajusta validação de senha e confirmação de senha para ter entre 8 e 12 caracteres

--- src/main/java/br/com/palm/matriculason/dtos/UsuariosDTO.java
+++ src/main/java/br/com/palm/matriculason/dtos/UsuariosDTO.java
@@ -1,13 +1,9 @@
 package br.com.palm.matriculason.dtos;
 
 import br.com.palm.matriculason.entities.Pessoas;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.postgresql.util.LruCache;
 
 import java.io.Serializable;
 
@@ -24,7 +20,7 @@ public class UsuariosDTO implements Serializable {
     @Size(min = 8, max = 12, message = "A senha deve ter entre 8 e 12 caracteres")
     private String senha;
 
-    @Size(min = 8, max = 12, message = "A confirmação de senha deve ter entre 8 e 12 caracteres")
-    @JsonIgnore
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 8, max = 12, message = "A senha deve ter entre 8 e 12 caracteres")
     private String confirmarSenha;
 }

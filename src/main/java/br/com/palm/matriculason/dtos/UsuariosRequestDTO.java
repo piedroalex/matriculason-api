1df453a30d8a7b3415b417fd84c9be5c61fa391d
Thalisson Moura <thalissonmoura138@gmail.com>
2025-01-20 11:00:25 -0300

    :sparkle:feat: adiciona classe de requisição pro cadastro de usuarios

---
+++ src/main/java/br/com/palm/matriculason/dtos/UsuariosRequestDTO.java
@@ -0,0 +1,22 @@
+package br.com.palm.matriculason.dtos;
+
+import br.com.palm.matriculason.entities.Pessoas;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class UsuariosRequestDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private Pessoas pessoa;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 8, max = 12, message = "A senha deve ter entre 8 e 12 caracteres")
+    private String senha;
+}
